be4d6f40a2e139f9c1a7411130d3f195f4f566c2
zhrren <18753794733@163.com>
2021-06-01 16:48:58 +0800

    本地清空

--- src/main/java/com/zr/TestA.java
+++ src/main/java/com/zr/TestA.java
@@ -7,11 +7,6 @@ package com.zr;
 public class TestA
 {
     public static void main(String[] args) {
-        System.out.println("11111111111");
-        System.out.println("22222222222");
-        System.out.println("33333333333");
-        System.out.println("dev");
-        System.out.println("dev");
-        System.out.println("master");
+
     }
 }
