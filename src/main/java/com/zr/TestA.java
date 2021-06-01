10d09a99c7ff81115f0e4ab916e17496d34e9d87
zhrren <18753794733@163.com>
2021-06-01 15:57:56 +0800

    第一次提交

---
+++ src/main/java/com/zr/TestA.java
@@ -0,0 +1,12 @@
+package com.zr;
+
+/**
+ * @Author: 张忍忍
+ * @Date: 2021/6/1 15:51
+ */
+public class TestA
+{
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+}
