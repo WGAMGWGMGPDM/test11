1835b46d96a41491c1365b71ed3e9b770a36d165
zhrren <18753794733@163.com>
2021-06-01 16:54:28 +0800

    还原了部分文件

--- src/main/java/com/zr/TestA.java
+++ src/main/java/com/zr/TestA.java
@@ -7,6 +7,11 @@ package com.zr;
 public class TestA
 {
     public static void main(String[] args) {
-
+        System.out.println("11111111111");
+        System.out.println("22222222222");
+        System.out.println("33333333333");
+        System.out.println("dev");
+        System.out.println("dev");
+        System.out.println("master");
     }
 }
